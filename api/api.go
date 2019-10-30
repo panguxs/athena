2e7179d7f33eb8a438d45e8016ad2dbc78c0a043
veily <drmfly.liw@gmail.com>
2019-10-30 15:13:52 +0800

    first commit

---
+++ api/api.go
@@ -0,0 +1,50 @@
+
+package api
+import(
+	"log"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	restfulspec "github.com/emicklei/go-restful-openapi"
+	"github.com/go-openapi/spec"
+)
+//Register register router and start api server
+func Register() {
+	u := UserResource{map[string]User{}}
+	restful.DefaultContainer.Add(u.WebService())
+
+	config := restfulspec.Config{
+		WebServices: restful.RegisteredWebServices(), // you control what services are visible
+		APIPath:     "/apidocs.json",
+		PostBuildSwaggerObjectHandler: enrichSwaggerObject}
+	restful.DefaultContainer.Add(restfulspec.NewOpenAPIService(config))
+
+	// Optionally, you can install the Swagger Service which provides a nice Web UI on your REST API
+	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
+	// Open http://localhost:8080/apidocs/?url=http://localhost:8080/apidocs.json
+	http.Handle("/apidocs/", http.StripPrefix("/apidocs/", http.FileServer(http.Dir("swagger-ui/dist"))))
+
+	log.Printf("start listening on localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+func enrichSwaggerObject(swo *spec.Swagger) {
+	swo.Info = &spec.Info{
+		InfoProps: spec.InfoProps{
+			Title:       "UserService",
+			Description: "Resource for managing Users",
+			Contact: &spec.ContactInfo{
+				Name:  "panguxs",
+				Email: "support@panguxs.io",
+				URL:   "https://panguxs.io",
+			},
+			License: &spec.License{
+				Name: "MIT",
+				URL:  "http://mit.org",
+			},
+			Version: "1.0.0",
+		},
+	}
+	swo.Tags = []spec.Tag{spec.Tag{TagProps: spec.TagProps{
+		Name:        "users",
+		Description: "Managing users"}}}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/panguxs/palladis
+
+go 1.13
+
+require (
+	github.com/emicklei/go-restful v2.11.1+incompatible
+	github.com/emicklei/go-restful-openapi v1.2.0
+	github.com/go-openapi/spec v0.19.4
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/julienschmidt/httprouter v1.2.0
+)
